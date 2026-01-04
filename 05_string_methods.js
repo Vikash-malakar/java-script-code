17ec0d1a0cb57b20f433d6b6c2f4bb3690fe4e2f
Vikash Malakar <vikashmalakar312@gmail.com>
2026-01-04 10:00:00 +0530

    javascript/practice

---
+++ 05_string_methods.js
@@ -0,0 +1,8 @@
+let text = "JavaScript Programming";
+
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.includes("Script"));
+console.log(text.startsWith("Java"));
+console.log(text.endsWith("ing"));
+console.log(text.slice(0, 10));
