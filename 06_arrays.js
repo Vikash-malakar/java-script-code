5b811135c747eb1ba47bc22278e1e340081aa933
Vikash Malakar <vikashmalakar312@gmail.com>
2026-01-05 10:00:00 +0530

    javascript/practice

---
+++ 06_arrays.js
@@ -0,0 +1,11 @@
+let fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits.length);
+
+fruits.push("Grapes");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
