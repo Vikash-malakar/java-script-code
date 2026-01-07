d774e238eaace0362d7415946704202f5b46f23f
Vikash Malakar <vikashmalakar312@gmail.com>
2026-01-07 10:00:00 +0530

    javascript/practice

---
+++ 08_objects.js
@@ -0,0 +1,10 @@
+let student = {
+    name: "Vikash",
+    age: 22,
+    course: "BCA",
+    city: "Bhopal"
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.course);
