9786ada0de20c7104aea39ee110f72830ee7e6a8
Vikash Malakar <vikashmalakar312@gmail.com>
2026-01-19 10:00:00 +0530

    javascript/practice

---
+++ 20_find.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Vikash",
+    age: 22,
+    course: "BCA"
+};
+
+let { name, age, course } = student;
+
+console.log(name);
+console.log(age);
+console.log(course);
---
+++ 21_destructuring.js
@@ -0,0 +1,7 @@
+// let numbers = [10, 20, 30, 40, 50];
+
+// let result = numbers.find(function (num) {
+//     return num > 25;
+// });
+
+// console.log(result);
