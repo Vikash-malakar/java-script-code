032e9ff76c4796c5a715e83e3cba8ce10065f45c
Vikash Malakar <vikashmalakar312@gmail.com>
2026-01-20 10:00:00 +0530

    javascript/practice

--- 21_destructuring.js
+++ 21_destructuring.js
@@ -1,7 +1,7 @@
-// let numbers = [10, 20, 30, 40, 50];
+let numbers = [10, 20, 30, 40, 50];
 
-// let result = numbers.find(function (num) {
-//     return num > 25;
-// });
+let result = numbers.find(function (num) {
+    return num > 25;
+});
 
-// console.log(result);
+console.log(result);
