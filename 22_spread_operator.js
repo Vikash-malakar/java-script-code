1deabd29cb3061d3e72fd3f35dc420ce50351f74
Vikash Malakar <vikashmalakar312@gmail.com>
2026-01-21 10:00:00 +0530

    javascript/practice

--- 22_spread_operator.js
+++ 22_spread_operator.js
@@ -1,6 +1,6 @@
-// let arr1 = [1, 2, 3];
-// let arr2 = [4, 5, 6];
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
 
-// let result = [...arr1, ...arr2];
+let result = [...arr1, ...arr2];
 
-// console.log(result);
+console.log(result);
