346c18829364fe75615aed2c050c089533e4bf49
Vikash Malakar <vikashmalakar312@gmail.com>
2026-01-26 10:00:00 +0530

    javascript/practice

---
+++ 26_math.js
@@ -0,0 +1,9 @@
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.1));
+
+console.log(Math.max(10, 20, 30));
+console.log(Math.min(10, 20, 30));
+
+console.log(Math.sqrt(25));
+console.log(Math.pow(2, 3));
