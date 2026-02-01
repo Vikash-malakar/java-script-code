b2caa1cfdc84388e0b39be8163532b1b13d4bcd6
Vikash Malakar <vikashmalakar312@gmail.com>
2026-02-01 10:00:00 +0530

    javascript/practice

---
+++ 02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "Vikash";
+let age = 22;
+let isStudent = true;
+let value = null;
+let data;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof value);
+console.log(typeof data);
